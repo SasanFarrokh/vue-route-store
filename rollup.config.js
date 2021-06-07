3b255dd66de55701894bb37566a00fd85ba71ca6
Sasan Farrokh <s.farrokh@alibaba.ir>
2021-06-07 21:48:39 +0430

    fix: migrate to rollup

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import typescript from 'rollup-plugin-ts';
+
+const config = {
+    input: './src/index.ts',
+    plugins: [
+        typescript({})
+    ]
+};
+
+export default [
+    {
+        ...config,
+        output: {
+            file: 'dist/index.js',
+            format: 'cjs',
+        }
+    },
+    {
+        ...config,
+        output: {
+            file: 'dist/index.esm.js',
+            format: 'esm',
+        },
+        plugins: [
+            typescript({ tsconfig: cnf => ({ ...cnf, declaration: false }) })
+        ]
+    }
+];
